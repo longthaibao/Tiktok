035955c6f7f1662e0f6e81b0ef0a636f7fced5bf
Thái Bảo Long <subin27102003@gmail.com>
2023-10-13 14:16:12 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import Home from "~/pages/Home";
+import HeaderOnly from "~/components/layout/HeaderOnly";
+import Following from "~/pages/Following";
+import Profile from "~/pages/Profile";
+import Upload from "~/pages/Upload";
+import Search from "~/pages/Search";
+export const publicRoutes = [
+  { path: "/", component: Home },
+  { path: "/following", component: Following },
+  { path: "/profile", component: Profile },
+  { path: "/upload", component: Upload, layout: HeaderOnly },
+  { path: "/search", component: Search, layout: null },
+];
+export const privateRoutes = [];
